6246ea4fbcad7f2f2d07d8743ca8e6f6fbf316de
Tijmen Stor <tijmen.stor@outlook.com>
2019-03-12 19:16:00 +0100

    Added rocket for REST

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+use rocket::http::RawStr;
+
+/// Returns
+
+#[get("/<id>")]
+fn get_tennis_flashcard(id: &RawStr) -> String {
+    format!("Your sent ID: {}", id.as_str())
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![get_tennis_flashcard]).launch();
 }
