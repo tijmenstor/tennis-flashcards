feaefcb931f76d4e7ced004271d6c94a915c6042
Tijmen Stor <tm.stor@outlook.com>
2020-02-08 23:33:38 +0100

    Update whole project. Start working on separate routes to serve info from JSON.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tennis Flashcards
+
+Tennis flashcards is a project currently consisting of a Rust backend with the Rocket framework to serve tennis-related questions. Frontend is yet to be decided.
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,34 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
-use rocket::http::RawStr;
+extern crate serde_json;
 
-/// Returns
+use std::fs::File;
+use std::io::Read;
 
-#[get("/<id>")]
-fn get_tennis_flashcard(id: &RawStr) -> String {
-    format!("Your sent ID: {}", id.as_str())
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+// Remove all unwraps at some point to not have any panics!
+// https://stackoverflow.com/questions/36362020/what-is-unwrap-in-rust-and-what-is-it-used-for
+#[get("/question/<question_id>")]
+fn get_question_by_id(question_id: usize) -> String {
+    let questions_json = load_json();
+    let question = &questions_json[question_id]["question"];
+    let question_type = &questions_json[question_id]["question_type"];
+    format!("Your question type is: {} and the question is: {}", question_type, question)
+}
+
+fn load_json() -> serde_json::Value {
+    let mut file = File::open("./questions.json").unwrap();
+    let mut data = String::new();
+    file.read_to_string(&mut data).unwrap();
+
+    return serde_json::from_str(&data).expect("JSON was wrongly formatted!");
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![get_tennis_flashcard]).launch();
+    rocket::ignite().mount("/", routes![index, get_question_by_id]).launch();
 }
